c6a0a9c5e6175d754ce58f0db1cc36b844bfca9a
venki712 <venky10ch95@gmail.com>
2026-07-18 11:55:36 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+function login(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('input[type="email"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    if (email === "" || password === "") {
+        alert("Please enter your email and password.");
+        return;
+    }
+
+    alert("Login Successful!");
+
+    window.location.href = "dashboard.html";
+}
